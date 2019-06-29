507ff4f0b4a6a88a21d4cb2b8fcfbf62365177a4
natekong <yichunko@usc.edu>
2019-06-29 19:03:13 +0800

    implement registration

--- playApp/forms.py
+++ playApp/forms.py
@@ -11,3 +11,19 @@ class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128,widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=256,widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     captcha = CaptchaField(label='验证码')
+
+class RegisterForm(forms.Form):
+    gender = (
+        ('male', "male"),
+        ('female', "female"),
+    )
+    username = forms.CharField(label="Username", max_length=128,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Password", max_length=256,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Confirm Password", max_length=256,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="Email",
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    sex = forms.ChoiceField(label='Sex', choices=gender)
+    captcha = CaptchaField(label='captcha')
--- playApp/views.py
+++ playApp/views.py
@@ -44,6 +44,9 @@ def search(request):
 
 
 def login(request):
+    if request.session.get('is_login', None):
+        return HttpResponseRedirect('/index')
+
     if request.method == "POST":
         login_form = UserForm(request.POST)
         message = "请检查填写的内容！"
@@ -53,6 +56,9 @@ def login(request):
             try:
                 user = models.Users.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
                     return HttpResponseRedirect('/index/')
                 else:
                     message = "密码不正确！"
@@ -70,5 +76,51 @@ def register(request):
 
 
 def logout(request):
-    pass
-    return HttpResponseRedirect('/index/')
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return HttpResponseRedirect("/index/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return HttpResponseRedirect("/index/")
+
+
+def register(request):
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册。你可以修改这条原则！
+        return HttpResponseRedirect("/index/")
+    if request.method == "POST":
+        register_form = RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, '../templates/register.html', locals())
+            else:
+                same_name_user = models.Users.objects.filter(name=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, '../templates/register.html', locals())
+                same_email_user = models.Users.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, '../templates/register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = models.Users.objects.create()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return HttpResponseRedirect('/login/')  # 自动跳转到登录页面
+    register_form = RegisterForm()
+    return render(request, '../templates/register.html', locals())
